59184a7b2e9707308e02ae7e3726e83bc6d403d9
shavlakmax <17031997>
2021-04-08 14:56:40 +0300

    Loader ux/ui updates

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -9,42 +9,37 @@ import Loader from '../loader/Loader';
 const Dashboard = props => {
   const [showInvitePopup, setShowInvitePopup] = useState(false);
 
+  if (props.loading) return  <Loader />
+
   return (
     <>
-      {
-        !props.loading ?
-          <>
-            {
-              props.customers.length ?
-                <div className="dashboard-content-header">
-                  <h3 className="dashboard-title">{`Welcome, ${props.user.first_name} ${props.user.last_name}!`}</h3>
-                  <CustomButton
-                    label="Invite Team Manager"
-                    handleClick={() => setShowInvitePopup(true)}
-                  />
-                </div> : null
-            }
-            {
-              props.customers.length ?
-                <CustomersTable
-                  customers={props.customers}
-                  showAssessments={props.showAssessments}
-                /> :
-                <EmptyDashboard
-                  handleShowPopup={() => setShowInvitePopup(true)}
-                />
-            }
-            {
-              showInvitePopup &&
-              <InviteTeamPopup
-                handleClosePopup={() => setShowInvitePopup(false)}
-                addCustomers={props.addCustomers}
-              />
-            }
-          </> : <Loader />
+      {props.customers.length ? (
+        <>
+          <div className="dashboard-content-header">
+            <h3 className="dashboard-title">{`Welcome, ${props.user.first_name} ${props.user.last_name}!`}</h3>
+            <CustomButton
+              label="Invite Team Manager"
+              handleClick={() => setShowInvitePopup(true)}
+            />
+          </div>
+          <CustomersTable
+            customers={props.customers}
+            showAssessments={props.showAssessments}
+          />
+        </>
+      ) : (
+        <EmptyDashboard
+          handleShowPopup={() => setShowInvitePopup(true)}
+        />
+      )}
+      {showInvitePopup &&
+        <InviteTeamPopup
+          handleClosePopup={() => setShowInvitePopup(false)}
+          addCustomers={props.addCustomers}
+        />
       }
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
