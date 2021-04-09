50187862597b121fa89d32108c8678969b64708f
shavlakmax <17031997>
2021-04-09 20:43:33 +0300

    admin dashbooard ux/ui

--- src/components/admin/CustomersTable.js
+++ src/components/admin/CustomersTable.js
@@ -1,81 +1,81 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Pagination from '../common/Pagination';
+import Table from '../common/Table';
+import RowMenu from './CustomersTableRowMenu';
 
 import './CustomersTable.scss';
 
-const CustomersTable = props => {
-  const [page, setPage] = React.useState(0);
-  const rowsPerPage = 10;
-
-  const handleChangePage = (event, newPage) => setPage(newPage);
-
-  const currentCustomersPage = props.customers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
-
-  const setAssessmentRisk = (customer, riskName) => {
-    if (customer.assessments_risk_list) {
-      let risk = customer.assessments_risk_list.find(risk => risk.assessment.split(' ')[0] === riskName);
-
-      if (risk && risk.risk_value) {
-        if (risk.risk_value <= 33) {
-          return <span className="have-assessment high">High risk</span>
-        } else if (risk.risk_value > 33 && risk.risk_value <= 66) {
-          return <span className="have-assessment medium">Medium risk</span>
-        } else if (risk.risk_value > 66) {
-          return <span className="have-assessment low">Low risk</span>
-        }
-      } else {
-        return <span>Incomplete</span>
-      }
-    } else {
-      return <span>Incomplete</span>
-    }
+const headers = [
+  {
+    title: 'Admin Name',
+    width: '18%',
+  },
+  {
+    title: 'Company Name',
+    width: '18%',
+  },
+  {
+    title: 'Startup Admin',
+    width: '15%',
+  },
+  {
+    title: 'CRL Assesment',
+    width: '15%',
+  },
+  {
+    title: 'TRL Assesment',
+    width: '15%',
+  },
+  {
+    title: 'MRL Assesment',
+    width: '15%',
+  },
+  {
+    width: '4%'
   }
+];
+
+
+const CustomersTable = ({ customers }) => {
+  const rows = mapCustomersToRow(customers)
 
   return (
     <div className="customers-table">
-      <div className="customers-table-head">
-        <ul>
-          <li>Company name</li>
-          <li>CRL assessment</li>
-          <li>TRL assessment</li>
-          <li>MRL assessment</li>
-        </ul>
-      </div>
-      <div className="customers-table-body">
-        <ul>
-          {
-            currentCustomersPage.map(customer =>
-              <li key={customer.id} className="customers-table-body-row">
-                <ul>
-                  <li>
-                    <Link 
-                      to={`/assessments/${customer.id}`}
-                      className="customers-link"
-                    >
-                      {customer.company_name}
-                    </Link>
-                  </li>
-                  <li>{setAssessmentRisk(customer, 'CRL')}</li>
-                  <li>{setAssessmentRisk(customer, 'TRL')}</li>
-                  <li>{setAssessmentRisk(customer, 'MRL')}</li>
-                </ul>
-              </li>
-            )
-          }
-        </ul>
-        <Pagination
-          rowsCount={props.customers.length}
-          currentRows={currentCustomersPage}
-          page={page}
-          rowsPerPage={rowsPerPage}
-          handleChangePage={handleChangePage}
-          itemsName="Customer"
-        />
-      </div>
+      <Table headers={headers} rows={rows} itemsName="companies" />
     </div>
-  )
-}
+  );
+};
 
-export default CustomersTable
+const mapCustomersToRow = customers => customers.map(customer => ({
+  id: customer.id,
+  row: [
+    'TODO',
+    <Link to={`/assessments/${customer.id}`} className="customers-link">
+      {customer.company_name}
+    </Link>,
+    'TODO',
+    formAssessmentRisk(customer, 'CRL'),
+    formAssessmentRisk(customer, 'TRL'),
+    formAssessmentRisk(customer, 'MRL'),
+    <RowMenu />,
+  ]
+}));
+
+const formAssessmentRisk = (customer, riskName) => {
+  const riskValue = customer.assessments_risk_list
+    ?.find(risk => risk.assessment.split(' ')[0] === riskName)
+    ?.risk_value;
+
+  if (!riskValue) return <span className="no-assessment">Incomplete</span>
+
+  if (riskValue <= 33) {
+    return <span className="have-assessment high">High risk</span>
+  } else if (riskValue > 33 && riskValue <= 66) {
+    return <span className="have-assessment medium">Medium risk</span>
+  } else {
+    return <span className="have-assessment low">Low risk</span>
+  }
+};
+
+export default CustomersTable;
---
+++ src/components/admin/CustomersTableRowMenu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { ReactComponent as ShowMoreIcon } from '../../images/icons/more-vertical.svg';
+
+import './CustomersTableRowMenu.scss';
+
+const CustomersTableRowMenu = () => {
+  const [showMenu, setShowMenu] = React.useState();
+
+  const toggleShowMenu = () => setShowMenu(!showMenu);
+
+  return (
+    <div className="menu-actions">
+      {showMenu &&
+        <div className="menu-actions-popup">
+          <span className="menu-action">
+            Edit
+          </span>
+          <span className="menu-action">
+            Delete
+          </span>
+        </div>
+      }
+      <ShowMoreIcon className="menu-actions-icon" onClick={toggleShowMenu} />
+    </div>
+  );
+};
+
+export default CustomersTableRowMenu;
