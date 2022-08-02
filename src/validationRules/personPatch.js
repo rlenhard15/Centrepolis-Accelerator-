b0d358947b278190fe530f3c46775e1f4d4d8e70
hogggy <will@mxv.vc>
2022-08-02 14:41:01 -0700

    allow more plus

--- src/validationRules/personPatch.js
+++ src/validationRules/personPatch.js
@@ -14,7 +14,7 @@ export default function validate(values) {
   if (!values.email) {
     errors.email = true;
     errors.emailError = '* is required';
-  } else if (!/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/.test(values.email)) {
+  } else if (!/^([A-Za-z0-9_\-.+])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/.test(values.email)) {
     errors.email = true;
     errors.email_message = 'Email address is invalid';
   }
